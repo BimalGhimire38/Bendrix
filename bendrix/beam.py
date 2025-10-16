e406d9bd58046c218420c820488dd0829e50c250
BimalGhimire38 <bimalghimire116@gmail.com>
2025-10-16 09:35:35 +0545

    initial commit

---
+++ bendrix/beam.py
@@ -0,0 +1,17 @@
+class SimplySupported:
+    pass
+
+class Cantilever:
+    pass
+
+class Fixed:
+    pass
+
+class Continuous:
+    pass
+
+class Overhanging:
+    pass
+
+class ProppedCantilever:
+    pass
