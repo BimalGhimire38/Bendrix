33ea5325b6d6db88fb265adc65d72bedf717c198
BimalGhimire38 <bimalghimire116@gmail.com>
2025-10-16 19:18:48 +0545

    updated loads

--- bendrix/loads.py
+++ bendrix/loads.py
@@ -2,9 +2,11 @@ class Loads:
     def __init__(self):
         pass
 
-    def point_load(self,magnitude,position):
+    def point_load(self,magnitude,cw_angle_to_left, position):
         self.magnitude = magnitude
-        self.position = position 
+        self.position = position
+        self.cw_angle_to_left = cw_angle_to_left
+        
         return self.magnitude, self.position
     
     def udl(self,magnitude,start,end):
@@ -12,3 +14,14 @@ class Loads:
         self.start = start
         self.end = end
         return self.magnitude, self.start, self.end
+    
+    def uvl(self,start_magnitude,end):
+        self.start_magnitude = start_magnitude
+        self.end = end
+        return self.start_magnitude, self.end
+    
+    def moment(self,magnitude,position):
+        self.magnitude = magnitude 
+        self.position = position
+        return self.magnitude, self.position
+    
